33cce0ab95f9b938090b54536feea89bab078163
Bladimir Antonio Ventura Paniagua <vladimirantonioventura6@gmail.com>
2025-05-22 16:45:27 -0400

    Delete Challenge Conversor de Monedas directory

--- Challenge Conversor de Monedas/src/Moneda/MonedasContact.java
+++
@@ -1,9 +0,0 @@
-package Moneda;
-
-import com.google.gson.annotations.SerializedName;
-import java.util.Map;
-
-public record MonedasContact(
-        @SerializedName("conversion_rates") 
-        Map<String, Double> conversionRates
-) {}
